434bdb8f6075cabb8547fde44ca25103cab83c07
rrTong <rrtong@ucdavis.edu>
2023-02-05 11:25:48 -0800

    create Landing, add basic style to Landing

---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "../styles/Landing.css";
+
+const Landing = () => {
+  return (
+    <div id="landing">
+      <div id="title">kylie's boosting service</div>
+    </div>
+  );
+};
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+import Landing from "./components/Landing";
 import "./index.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>kylie's boosting service</h1>
+      <Landing />
     </div>
   );
 }
