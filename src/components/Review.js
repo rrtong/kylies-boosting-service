d96938c37d4a46574af23d694f6b18fae3065e30
rrTong <rrtong@ucdavis.edu>
2023-02-09 02:03:24 -0800

    send picture thru props

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,10 +1,9 @@
 import "../styles/Review.css";
-import HsinChao from "../assets/hsinchao.png";
 
-const Review = () => {
+const Review = (revProps) => {
   return (
     <div id="review">
-      <img className="reviewPicture" src={HsinChao} />
+      <img className="reviewPicture" src={revProps.reviewPicture} />
       <div className="reviewName">Hsin Chao-Wen</div>
       <div className="reviewRating">5 out of 5 stars</div>
       <div className="reviewDescription">
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Review from "../components/Review";
 import "../styles/Reviews.css";
+import HsinChao from "../assets/hsinchao.png";
 
 const Reviews = () => {
   return (
     <div id="reviews">
       <div className="title">Our satisfied boostees</div>
       <div id="reviewCarousel">
-        <Review />
-        <Review />
-        <Review />
-        <Review />
-        <Review />
-        <Review />
-        <Review />
+        <Review reviewPicture={HsinChao} />
+        <Review reviewPicture={HsinChao} />
+        <Review reviewPicture={HsinChao} />
+        <Review reviewPicture={HsinChao} />
+        <Review reviewPicture={HsinChao} />
+        <Review reviewPicture={HsinChao} />
+        <Review reviewPicture={HsinChao} />
+        <Review reviewPicture={HsinChao} />
       </div>
     </div>
   );
