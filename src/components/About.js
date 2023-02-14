6d9c4439403b9994030fd02b794f5ebce118e7ae
rrTong <rrtong@ucdavis.edu>
2023-02-13 22:53:58 -0800

    meet the team

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Teammate from "../components/Teammate";
+import rammus from "../assets/rammus.png";
 import "../styles/About.css";
 
 const About = () => {
   return (
     <div id="about">
       <div className="title">meet the team!</div>
+      <div id="team">
+        <Teammate
+          pic={rammus}
+          name={'Kylie Chao-wen "quyxx" Hsu'}
+          role={"CEO"}
+        />
+      </div>
     </div>
   );
 };
---
+++ src/components/Teammate.js
@@ -0,0 +1,15 @@
+import "../styles/Teammate.css";
+
+const Teammate = (props) => {
+  return (
+    <div id="teammate">
+      <div className="teammate-pic">
+        <img src={props.pic} alt="kylie"></img>
+      </div>
+      <div className="teammate-name">{props.name}</div>
+      <div className="teammate-role">{props.role}</div>
+    </div>
+  );
+};
+
+export default Teammate;
