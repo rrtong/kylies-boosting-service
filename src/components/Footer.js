8c4ae55a438336ac544feaf05b8f85c8104a7325
rrTong <rrtong@ucdavis.edu>
2024-04-17 16:21:12 -0700

    update copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <div id="footer">
-      <div>© 2023 KBS Inc. All rights reserved.</div>
+      <div>© 2024 KBS Inc. All rights reserved.</div>
       <a href={"mailto:soliloquyxx@kbs.com"} target="_blank">
         <div id="subfooter">Questions? Email us!</div>
       </a>
