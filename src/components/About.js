1c95b85a6661386216112f78d724205788a58daa
rrTong <rrtong@ucdavis.edu>
2024-04-17 16:16:59 -0700

    update profile photos

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,12 @@ import rammus_chrik from "../assets/rammus_chrik.png";
 import rammus_ryan from "../assets/rammus_ryan.png";
 import rammus_frank from "../assets/rammus_frank.png";
 import rammus_reanemone from "../assets/rammus_reanemone.png";
+import rammus_ammeck from "../assets/rammus_ammeck.png";
+import rammus_alya from "../assets/rammus_alya.png";
+import rammus_wnedy from "../assets/rammus_wnedy.png";
+import rammus_ryuu from "../assets/rammus_ryuu.png";
+import rammus_pilocase from "../assets/rammus_pilocase.png";
+
 import "../styles/About.css";
 
 const About = () => {
@@ -23,9 +29,7 @@ const About = () => {
       </div>
       <div id="subordinates">
         <Teammate
-          pic={
-            "https://cdn.discordapp.com/guilds/948856567871209472/users/746233231405482015/avatars/e8dd49823d6918205610e1f517a3cbcd.webp?size=256"
-          }
+          pic={rammus_ryuu}
           url={"https://www.tiktok.com/@ryuu.tv"}
           name={'Annie "Ryuu" Queen'}
           role={"CMO"}
@@ -37,9 +41,7 @@ const About = () => {
           role={"CFO"}
         />
         <Teammate
-          pic={
-            "https://cdn.discordapp.com/guilds/948856567871209472/users/190566141507141632/avatars/f7fdfc13b7f85241e1e639409829d6de.webp?size=256"
-          }
+          pic={rammus_pilocase}
           url={"https://www.twitch.tv/pilocase"}
           name={'Mr. Andrew "pilocase" Gamer'}
           role={"Senior Principal Executive Booster"}
@@ -69,25 +71,19 @@ const About = () => {
           role={"Booster Intern"}
         />
         <Teammate
-          pic={
-            "https://cdn.discordapp.com/avatars/444363277787725834/9777cd4a7ee3ca4ac0ebe71866daf1d5.webp?size=256"
-          }
+          pic={rammus_wnedy}
           url={"https://www.deviantart.com/wndyxxox"}
           name={'Wnedy "Shuuri" Meishan'}
           role={"Booster Intern"}
         />
         <Teammate
-          pic={
-            "https://cdn.discordapp.com/avatars/330254340482793472/bf1e0ba20c9c89e9960b6a9de33b9086.webp?size=256"
-          }
+          pic={rammus_ammeck}
           url={"https://steamcommunity.com/id/momonobadgirltimeout"}
           name={'Just "Ammeck" n0k1'}
           role={"Booster Intern"}
         />
         <Teammate
-          pic={
-            "https://cdn.discordapp.com/guilds/948856567871209472/users/190657628928606208/avatars/4c3611a8248c1cd021f42d8a161343dd.webp?size=256"
-          }
+          pic={rammus_alya}
           url={"https://www.twitch.tv/hikarinyui"}
           name={'Emma-Lee "Alya" Marie'}
           role={"Booster Intern"}
